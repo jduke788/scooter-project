eab47a9fdd2c462c383c7a9cff62b021614a1fbd
jduke788 <jduke788@gmail.com>
2024-10-04 12:15:01 +0100

    Scooter complete not tested

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,27 @@
 class Scooter {
   // scooter code here
+  static nextSerial = 1 
+  constructor(station) {
+    this.station = station
+    this.user = null
+    this.serial = Scooter.nextSerial ++
+    this.charge = 100
+    this.isBroken = false
+  }
+  rent(user) {
+    if (this.charge > 20 && this.isBroken === false) {
+      this.station = null
+      this.user = user 
+          } else if (this.charge <= 20 && this.isBroken === false)
+    throw new Error('Scooter need to charge')
+     else if (this.charge > 20 && this.isBroken === true) {
+      throw new Error('Scooter needs repair')
+    }
+   } dock(station) {
+      this.station = station
+      this.user = null
+
+    }
 }
 
 module.exports = Scooter
